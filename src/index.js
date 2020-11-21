40f6460bf814c3befd243513d3f0279f7786f2c6
jonacruz948 <jonacruz948@gmail.com>
2020-11-21 20:42:39 +0100

    cors allow all

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.options('*', cors());
 
 app.use(function (req, res, next) {
-  //to allow cross domain requests to send cookie information.
+  // to allow cross domain requests to send cookie information.
   res.header('Access-Control-Allow-Credentials', true);
 
   // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Origin', '*');
 
   // list of methods that are supported by the server
   res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
