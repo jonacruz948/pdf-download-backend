01c09e864339d8ebddf1c099696b0c99d6f2da1e
jonacruz948 <jonacruz948@gmail.com>
2020-11-21 20:53:57 +0100

    res.headers removed

--- src/index.js
+++ src/index.js
@@ -9,25 +9,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.options('*', cors());
 
-app.use(function (req, res, next) {
-  // to allow cross domain requests to send cookie information.
-  // res.header('Access-Control-Allow-Credentials', true);
+// app.use(function (req, res, next) {
+//   // to allow cross domain requests to send cookie information.
+//   // res.header('Access-Control-Allow-Credentials', true);
 
-  // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-  res.header('Access-Control-Allow-Origin', '*');
+//   // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
+//   res.header('Access-Control-Allow-Origin', '*');
 
-  // list of methods that are supported by the server
-  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+//   // list of methods that are supported by the server
+//   res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
 
-  res.header(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, X-XSRF-TOKEN'
-  );
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, X-XSRF-TOKEN'
+//   );
 
-  next();
-});
+//   next();
+// });
 
 app.use('/', routes);
 
